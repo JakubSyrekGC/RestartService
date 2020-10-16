6ea026a0a79f5711dab2ed24e245f878d7271785
JakubSyrekGC <71333889+JakubSyrekGC@users.noreply.github.com>
2020-10-16 18:43:16 +0200

    Update Check-Services.ps1

--- Check-Services.ps1
+++ Check-Services.ps1
@@ -1,11 +1,9 @@
-﻿USING MODULE .\ServicesHelpers.psm1
+USING MODULE .\ServicesHelpers.psm1
 
-#$erroractionpreference = "SilentlyContinue" 
 $ServersList = $ENV:SERVERS_LIST
 $Username    = $ENV:ADMIN
 $Password              = ConvertTo-SecureString $ENV:PASS -AsPlainText -Force 
 $Credentials           = New-Object System.Management.Automation.PSCredential ($Username, $Password)
-#EndRegion Consts
 $Result = @()
 
 foreach ($srv in $ServersList) 
@@ -17,6 +15,6 @@ foreach ($srv in $ServersList)
 
 ($Result | Format-Table -Wrap -Property ServerName,MetaServer,MetaAdminAPIService,MetaRatesCenter,MetaRefRateIndicator,TibRVD  | Out-String )  
 
-if([Functions]::ExportHtmlFile( $Result , "C:\___\OutputG2MetaProc.html")) { Write-Host "Results exported to html" }
+if([Functions]::ExportHtmlFile( $Result , "D:\Scripts\PO\G2-Meta-App-Restart-Check\OutputG2MetaProc.html")) { Write-Host "Results exported to html" exit 0 }else{exit 1}
 
-exit 0
+
