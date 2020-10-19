1daf4585319bb4a73a0e9d4be86575f2083362e4
JakubSyrekGC <71333889+JakubSyrekGC@users.noreply.github.com>
2020-10-19 22:10:11 +0200

    Update Check-MtLogs.ps1

--- Check-MtLogs.ps1
+++ Check-MtLogs.ps1
@@ -4,20 +4,21 @@ $Console = $Host.UI.RawUI
 $Buffer = $Console.BufferSize  
 $Buffer.Width = '4096'
 $Console.BufferSize = $Buffer 
+$newLine = [Environment]::NewLine
+$line = Write Output "$newLine###################################################################################$newLine"
 
+$line
 $ServersList = $env:G2_Meta_Servers
 Write-Output "Servers: $ServersList"
+$line
 
 $Result = @()
-
 $Result = [Functions]::CheckMetaLogs($ServersList)
-
-
 
 if($Result -ne $null)
 {
      ($Result | Format-Table -Wrap | Out-String )
-    
+     $line
 #    if([Functions]::ExportHtmlFile( $Result , "D:\Scripts\PO\G2-Meta-App-Restart-Check\OutputG2MetaProc.html")) { Write-Host "Results exported to html"} 
 }
 else
