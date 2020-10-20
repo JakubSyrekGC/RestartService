a2fbd3442f3f4322c02c51bd50dd93df006c393b
JakubSyrekGC <71333889+JakubSyrekGC@users.noreply.github.com>
2020-10-20 15:30:26 +0200

    Update Check-MtLogs.ps1

--- Check-MtLogs.ps1
+++ Check-MtLogs.ps1
@@ -8,26 +8,35 @@ $newLine = [Environment]::NewLine
 $Properties = ("ServerName,TradeCont").Split(",")
 $HTMLpath = "$env:OutputsForMetaStack\G2-Meta-App-Restart-Check\OutputG2MetaProc.html"
 $HostName = [System.Net.Dns]::GetHostName()
+$bar = Write-Output "$newLine#####################################################################################################################$newLine"
 #Endregion Preconfigure
 
+$bar
 #Region Execute
-Write-Output $newLine#####################################################################################################################$newLine
 $ServersList = $env:G2_Meta_Servers
 Write-Output "Servers: $ServersList" 
-Write-Output $newLine#####################################################################################################################$newLine
 $Result = @()
 $Result = [Functions]::CheckMetaLogs($ServersList)
-if([Functions]::ExportHtmlFile( $Result, $HTMLpath, $Properties)) 
-  {Write-Output "HTML exported to $HostName / $HTMLpath"}
-else
-  {Write-Output "Error during HTML export" }
 #Endregion Execute
 
+$bar
+#Region ExportHTML
+if($Result -ne $null -and $Result[0].TradeCont.Contains("error") -ne $true) {
+  if([Functions]::ExportHtmlFile( $Result, $HTMLpath, $Properties)) 
+    {Write-Output "HTML exported to $HostName / $HTMLpath"}
+  else
+    {Write-Output "Error during HTML export" }
+}
+#EndRegion ExportHTML
+
+$bar
 #Region DisplayResults
 [Functions]::DisplayResults($Result, $Properties)
-Write-Output $newLine#####################################################################################################################$newLine
+
 #Endregion DisplayResults
 
+$bar
+#Region ReturnResultCodeFromScript
 if($Result -ne $null -and $Result[0].TradeCont.Contains("error") -ne $true) {
     Write-Output "Logs successfully verified"
     exit 0
@@ -36,3 +45,4 @@ else {
     Write-Output "Error! Logs not verified. Please check!"
     exit 1
 }
+#Endregion ReturnResultCodeFromScript
