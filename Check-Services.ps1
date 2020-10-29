1e38e478599b353eb7832825e3f45b0d90857f7d
JakubSyrekGC <71333889+JakubSyrekGC@users.noreply.github.com>
2020-10-29 17:00:00 +0100

    Update Check-Services.ps1

--- Check-Services.ps1
+++ Check-Services.ps1
@@ -1,3 +1,4 @@
+#Region Preconfigure
 USING MODULE .\ServicesHelpers.psm1
 
 $Console = $Host.UI.RawUI 
@@ -9,6 +10,11 @@ $ServersList = $ENV:MT5_Start.Split(",") #SERVERS_LIST
 $Username    = $ENV:ADMIN
 $Password              = ConvertTo-SecureString $ENV:PASS -AsPlainText -Force 
 $Credentials           = New-Object System.Management.Automation.PSCredential ($Username, $Password)
+
+$HTMLpath = "$env:OutputsForMetaStack\G2-Meta-App-Restart-Check\OutputG2MetaProc.html"
+#Endregion Preconfigure
+
+
 $Result = @()
 
 foreach ($srv in $ServersList) 
@@ -18,12 +24,35 @@ foreach ($srv in $ServersList)
     $Result += $metaserver     
 }   
 
-if([Functions]::ExportHtmlFile( $Result , "D:\Scripts\PO\G2-Meta-App-Restart-Check\OutputG2MetaProc.html", "ServerName,MetaServer,MetaAdminAPIService,MetaRatesCenter,MetaRefRateIndicator,TibRVD")) { Write-Host "Results exported to html"} 
 
+
+#Region ExportHTML
+if($Result -ne $null -and $Result[0] -ne $null) {  
+  
+  if([Functions]::ExportHtmlFile( $Result, $HTMLpath, ([Properties]::CheckMt5ServicesProps) )) 
+    {Write-Output "HTML exported to $HostName / $HTMLpath"}
+  else
+    {Write-Output "Error during HTML export" }
+}
+#EndRegion ExportHTML
+
+$bar
+#Region DisplayResults
 return ($Result | Format-Table -Wrap -Property ([Properties]::CheckMt5ServicesProps)  | Out-String )  
 
-exit 0
+#Endregion DisplayResults
 
+
+#Region ReturnResultCodeFromScript
+if($Result -ne $null -and $Result[0].TradeCont.Contains("error") -ne $true) {
+    Write-Output "Logs successfully verified $bar"
+    exit 0
+}
+else {
+    Write-Output "Error! Logs not verified. Please check! $bar"
+    exit 1
+}
+#Endregion ReturnResultCodeFromScript
 
 
 
