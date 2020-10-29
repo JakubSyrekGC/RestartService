26947ca418d11a080095e69a7198de5ba8368f50
JakubSyrekGC <71333889+JakubSyrekGC@users.noreply.github.com>
2020-10-29 18:37:54 +0100

    Update Check-G2MetaAppRestart.ps1

--- Check-G2MetaAppRestart.ps1
+++ Check-G2MetaAppRestart.ps1
@@ -1,27 +1,20 @@
 ﻿#Region Preconfigure
-USING MODULE C:\dev\ServicesHelpers.psm1
-#.\ServicesHelpers.psm1
-
-#Region Consts
-#$ServersList = $ENV:MT5_Start
-$ServersList = @("MT4104Demo")
-#$Username    = $ENV:ADMIN
-#$Password              = ConvertTo-SecureString $ENV:PASS -AsPlainText -Force 
-#$Credentials           = New-Object System.Management.Automation.PSCredential ($Username, $Password)
-
-
+USING MODULE .\ServicesHelpers.psm1
+$ServersList = $ENV:MT5_Start
+$Username    = $ENV:ADMIN
+$Password              = ConvertTo-SecureString $ENV:PASS -AsPlainText -Force 
+$Credentials           = New-Object System.Management.Automation.PSCredential ($Username, $Password)
 $HTMLpath               = "$env:OutputsForMetaStack\G2-Meta-App-Restart-Check\OutputG2MetaProc.html"
 #Endregion Preconfigure
 
 
 #Region Execute
-$Result = @()
-
+$Result = @();
 foreach ($srv in $ServersList) 
 {
     $metaserver = [MetaServerRestarter]::new($srv, $credentials);    
-    $metaserver.GetLastBootTime()
-    $Result += $metaserver     
+    $metaserver.GetLastBootTime();
+    $Result += $metaserver;     
 }
 #Endregion Execute
 
@@ -54,10 +47,3 @@ else {
     exit 1
 }
 #Endregion ReturnResultCodeFromScript
-
-
-($Result | Format-Table -Wrap -Property ( [Properties]::CheckMetaProps )  | Out-String )  
-
-#if([Functions]::ExportHtmlFile( $Result , "C:\___\OutputG2MetaProc.html")) { Write-Host "Results exported to html" }
-
-#exit 0
