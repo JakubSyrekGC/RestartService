e38de9bb732c3fed4ed4bea2e038bc25e327eab3
JakubSyrekGC <71333889+JakubSyrekGC@users.noreply.github.com>
2020-10-30 18:59:02 +0100

    Add files via upload

---
+++ Check-RcrStatus.ps1
@@ -0,0 +1,49 @@
+﻿#Region Preconfigure
+USING MODULE C:\dev\ServicesHelpers.psm1
+#.\ServicesHelpers.psm1
+
+#$ServersList = $ENV:MT5_Start
+$ServersList =                       @("INX-SRV-MTBR01")
+#$Username    = $ENV:ADMIN
+#$Password              = ConvertTo-SecureString $ENV:PASS -AsPlainText -Force 
+#$Credentials           = New-Object System.Management.Automation.PSCredential ($Username, $Password)
+
+$username               = “cityindex\adm.jak.syr”
+$password               = ConvertTo-SecureString “15Friday54!!” -AsPlainText -Force 
+$credentials            = New-Object System.Management.Automation.PSCredential ($username, $password)
+$HTMLpath               = "$env:OutputsForMetaStack\RCR_Status\OutputRCRStatus.html"
+#"C:\dev\OutputRCRStatus.html"
+
+#Endregion Preconfigure
+
+
+#Region Execute
+$Result = @()
+
+foreach ($srv in $ServersList) 
+{
+    $RCRChecker = [RCRChecker]::new($srv, $credentials);    
+    $RCRChecker.GetRcrStatus()
+    $Result += $RCRChecker     
+}
+#Endregion Execute
+
+
+#Region DisplayResults
+
+return ($Result | Format-Table -Wrap -Property ( [Properties]::CheckRCRProps ) | Out-String )  
+
+#Endregion DisplayResults
+
+
+
+#Region ExportHTML
+if($Result -ne $null -and $Result[0] -ne $null) {  
+  
+  if([Functions]::ExportHtmlFile( $Result, $HTMLpath, ( [Properties]::CheckRCRProps ))) 
+    {Write-Output "HTML exported to $HTMLpath"}
+  else
+    {Write-Output "Error during HTML export" }
+}
+#EndRegion ExportHTML
+
