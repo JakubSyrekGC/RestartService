c4cdbc4a440fc8e710c9fdf1c9be9c39fd17ddab
JakubSyrekGC <71333889+JakubSyrekGC@users.noreply.github.com>
2020-10-30 19:15:00 +0100

    Update Check-RcrStatus.ps1

--- Check-RcrStatus.ps1
+++ Check-RcrStatus.ps1
@@ -1,7 +1,7 @@
 ﻿#Region Preconfigure
 USING MODULE .\ServicesHelpers.psm1
 
-$ServersList           = $ENV:RCR_Servers
+$ServersList           = $ENV:RCR_Servers.Split(",")
 $Username              = $ENV:ADMIN
 $Password              = ConvertTo-SecureString $ENV:PASS -AsPlainText -Force 
 $Credentials           = New-Object System.Management.Automation.PSCredential ($Username, $Password)
